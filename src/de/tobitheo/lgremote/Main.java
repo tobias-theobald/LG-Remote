112faa87244fbd9b97161e9a4f0e464bd10caea3
Tobias Theobald <tobitheo@gmail.com>
2012-02-29 19:42:42 +0100

    Added the beginning of a GUI. Connected to nothing atm though.

--- src/de/tobitheo/lgremote/Main.java
+++ src/de/tobitheo/lgremote/Main.java
@@ -1,31 +1,126 @@
 package de.tobitheo.lgremote;
 
+import java.awt.ComponentOrientation;
+import java.awt.Container;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.net.UnknownHostException;
-import java.util.Scanner;
+
+import javax.swing.Icon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
 public class Main {
 
-	private static String authKey = "DDJOFF";
+	JFrame frame;
+	
+	public Main() {
 
-	public static void main(String[] args) throws UnknownHostException,
-			IOException, InterruptedException {
-		Connection conn = new Connection();
-		conn.mDnsDiscovery();
-		if (authKey == null) {
-			conn.showAuthKey();
-			System.out.println("Key should be displayed now");
-		} else
-			System.out.println(conn.authenticate(authKey));
+		// Create and set up the window.
+		frame = new JFrame("GridBagLayoutDemo");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		int start = 0;
-		Scanner in = new Scanner(System.in);
-		String s = in.nextLine();
-		while (s != "") {
-			conn.sendKeyCode(Integer.parseInt(s));
-			s = in.nextLine();
+		// Set up the content pane.
+		Container pane = frame.getContentPane();
+
+		JButton button;
+		pane.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+
+		ActionListener al = new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				Object o = arg0.getSource();
+				if (!(o instanceof KeyButton))
+					throw new IllegalStateException(
+							"That was not a key button. This ActionListener ist only for key buttons.");
+				KeyButton kb = (KeyButton) o;
+				// TODO Proper key press handling (aka send the command to the
+				// TV)
+				System.out.println(kb.getKey().toString());
+			}
+		};
+
+		c.fill = GridBagConstraints.BOTH;
+		c.weightx = 0.5;
+		c.ipady = 10;
+
+		c.gridx = 0;
+		c.gridy = 0;
+		// pane.add(button, c);
+
+		int i = 0;
+		for (Key key : Key.values()) {
+			button = new KeyButton(key);
+			c.gridx = i % 3;
+			c.gridy = i / 3;
+			button.addActionListener(al);
+			pane.add(button, c);
+			++i;
 		}
-			
-		
+//		button = new JButton("Button 2");
+//		c.gridx = 1;
+//		c.gridy = 0;
+//		pane.add(button, c);
+//
+//		button = new JButton("Button 3");
+//		c.gridx = 2;
+//		c.gridy = 0;
+//		pane.add(button, c);
+//
+//		button = new JButton("Long-Named Button 4");
+//		c.gridwidth = 3;
+//		c.gridx = 0;
+//		c.gridy = 1;
+//		pane.add(button, c);
+
+	}
+
+	private void show() {
+		// Display the window.
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		// Schedule a job for the event-dispatching thread:
+		// creating and showing this application's GUI.
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new Main().show();
+			}
+		});
+	}
+
+	@SuppressWarnings("unused")
+	private class KeyButton extends JButton {
+
+		private static final long serialVersionUID = -5785177244336835106L;
+		private final Key key;
+
+		public KeyButton(Key key, String text, Icon icon) {
+			super(text, icon);
+			this.key = key;
+		}
+
+		public KeyButton(Key key, String text) {
+			super(text);
+			this.key = key;
+		}
+
+		public KeyButton(Key key) {
+			super(key.toString());
+			this.key = key;
+		}
+
+		public Key getKey() {
+			return key;
+		}
+
 	}
 }
